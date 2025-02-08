e595eaa358722d411480185c3bd6eb25e9c134a4
Saurav Shinde <sauravshinde.000@gmail.com>
2025-02-08 23:22:39 +0530

    Added CBAM to EfficientNet-B3

---
+++ translations.py
@@ -0,0 +1,20 @@
+translations = {
+    "en": {
+        "title": "Crop Disease Detection",
+        "description": "Upload an image and get disease predictions along with remedies.",
+        "upload_label": "Upload Image",
+        "detect_button": "Detect Disease",
+    },
+    "hi": {
+        "title": "फसल रोग पहचान",
+        "description": "छवि अपलोड करें और रोग की भविष्यवाणी और उपचार प्राप्त करें।",
+        "upload_label": "छवि अपलोड करें",
+        "detect_button": "रोग पहचानें",
+    },
+    "mr": {
+        "title": "पिक रोग ओळख",
+        "description": "प्रतिमा अपलोड करा आणि रोगाचे निदान व उपचार मिळवा.",
+        "upload_label": "प्रतिमा अपलोड करा",
+        "detect_button": "रोग ओळखा",
+    }
+}
